639dfc675f61e9f7ea0fa5ebc9edc0b90a5c3405
jux42 <roujux@gmail.com>
2025-01-25 21:28:07 +0100

    commit from new env

--- src/CodeWars/Main.java
+++ src/CodeWars/Main.java
@@ -5,12 +5,5 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-        int count = 0;
-        while(count < 100000000){
-            System.out.println("count is " + count);
-            count++;
-        }
-
     }
 }
